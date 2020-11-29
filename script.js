1749cb413e61e5cc743d10f2d6fcefcea1889c6f
omayma ahmad <omayma.ahmad25@gmail.com>
2020-11-29 16:01:22 +0000

    set up local storage

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ $(document).ready(function () {
   });
 });
 
+
+// local storage 
+var citiesPreviouslySearched =
+    JSON.parse(localStorage.getItem("cities")) || [];
+
 // use API to gather the data
 function weatherData(search) {
   $.ajax({
@@ -19,25 +24,42 @@ function weatherData(search) {
   }).then(function (response) {
     console.log(response);
 
+// add to local storage 
+    if (!citiesPreviouslySearched.includes(search)) {
+      citiesPreviouslySearched.push(search);
+      localStorage.setItem("cities", JSON.stringify(citiesPreviouslySearched));
+    }
+
     weatherForecast(search);
     weatherUV(response.coord.lat, response.coord.lon);
   });
 }
 function weatherForecast(search) {
   $.ajax({
-    url: "http://api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=15a9f0e5a6e44b139bceaeb431391c1b",
+    url:
+      "http://api.openweathermap.org/data/2.5/forecast?q=" +
+      search +
+      "&appid=15a9f0e5a6e44b139bceaeb431391c1b",
     type: "GET",
   }).then(function (response) {
-      console.log("sdfsf")
+    console.log("sdfsf");
     console.log(response);
   });
 }
 
 function weatherUV(lat, lon) {
   $.ajax({
-    url: "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=15a9f0e5a6e44b139bceaeb431391c1b",
+    url:
+      "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=15a9f0e5a6e44b139bceaeb431391c1b",
     type: "GET",
   }).then(function (response) {
     console.log(response);
   });
+
+  var citiesPreviouslySearched =
+    JSON.parse(localStorage.getItem("cities")) || [];
 }
