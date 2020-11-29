6555c86b259ce5daad2cb9bb4b93344f7b92cab4
omayma ahmad <omayma.ahmad25@gmail.com>
2020-11-29 15:51:36 +0000

    set up ajax requests

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
-$(document).ready(function(){
-    $("#searchBtn").on("click", function() {
-        var search =$("#searchBtn").val();
-        console.log(search);
-    });
+//  Search Button
+$(document).ready(function () {
+  $("#searchBtn").on("click", function () {
+    var search = $("#searchInput").val();
+    console.log(search);
+
+    weatherData(search);
+  });
 });
+
+// use API to gather the data
+function weatherData(search) {
+  $.ajax({
+    url:
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      search +
+      "&appid=15a9f0e5a6e44b139bceaeb431391c1b",
+    type: "GET",
+  }).then(function (response) {
+    console.log(response);
+
+    weatherForecast(search);
+    weatherUV(response.coord.lat, response.coord.lon);
+  });
+}
+function weatherForecast(search) {
+  $.ajax({
+    url: "http://api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=15a9f0e5a6e44b139bceaeb431391c1b",
+    type: "GET",
+  }).then(function (response) {
+      console.log("sdfsf")
+    console.log(response);
+  });
+}
+
+function weatherUV(lat, lon) {
+  $.ajax({
+    url: "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=15a9f0e5a6e44b139bceaeb431391c1b",
+    type: "GET",
+  }).then(function (response) {
+    console.log(response);
+  });
+}
